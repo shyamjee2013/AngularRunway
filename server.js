177d77dd430e13dcd0ea2de1c48df4681529d246
shyamjee2013 <shyamjee2013@gmail.com>
2018-07-04 16:24:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,41 @@
  // default api
  app.get('/', function (request, response) {
     var data = [
-        { FirstTopic:"Angular" },
-        { SecondTopic:"TypeScript" },
-        { ThirdTopic: "Express JS" },
-        { FourthTopic:"Mongo Database" }
+        { DefaultApi:"http://localhost:8080/" },
+        { TopicToStudy: "http://localhost:8080/topic" },
+        { ForLogin:"http://localhost:8080/logincheck?user=amt2015&password=angular6" }        
     ]    
     console.log("Data coming from server..");
     return response.send(data);
      //return res.send({ error: true, message: 'hello' })
  });
 
-// Login api
+ app.get('/topic', function (request, response) {
+
+    var data =[{
+        "technology":
+                {
+                    "angular":
+                        [
+                            { "id": "101", "topic": "Log Out Session" },
+                            { "id": "102", "topic": "Token Authentication" },
+                            { "id": "103", "topic": "Data Insertion in Mongo" },
+                            { "id": "104", "topic": "Stored Procedure calling in Mongo" },
+                            { "id": "105", "topic": "Dropdown binding" },
+                            { "id": "106", "topic": "Reset Password" },
+                            { "id": "107", "topic": "Forms and Template Driven Form" },
+                            { "id": "108", "topic": "Angular Life Cycle" },
+                            { "id": "109", "topic": "Parent Child Relation-ship" },
+                            { "id": "110", "topic": "@Input/ Output" },
+                            { "id": "111", "topic": "Angular Life Cycle" },
+                            { "id": "112", "topic": "Hosting Angular Application and Express JS Service and Mongo Database" }
+                        ]
+                }
+    }]
+    return response.send(data);
+});
+
+  // Login api
   app.get('/logincheck', function (request, response) {
       debugger;
     var loginUserName = request.query.user;
@@ -31,23 +55,10 @@
 
     if(loginUserName == 'amt2015' && loginUserPassword == 'angular6')
     {
-
-        var data =[{
-            "technology":
-                    {
-                        "angular":
-                            [
-                                { "id": "1001", "topic": "Component" },
-                                { "id": "1002", "topic": "Directives" },
-                                { "id": "1003", "topic": "2 way Binding" },
-                                { "id": "1004", "topic": "NgModel" },
-                                { "id": "1005", "topic": "Directives" },
-                                { "id": "1006", "topic": "Angular Life Cycle" },
-                                { "id": "1007", "topic": "Pipes" }                                
-                            ]
-                    }
-        }]
-        return response.send({ error: true, message: 'Login sucess..',data })
+        var data =[
+            { username:"amt2015",Password:"angular6",profession:"software developer"}
+        ]
+        return response.send(data);
     }
     else
     {
@@ -55,8 +66,8 @@
         return response.send({ error: true, message: 'Login failed...wrong UserName and Password.' })
     }
  });
-      
 
+ 
  app.listen(8080, function () {
     console.log('Node app is running on port 8080');
 });
